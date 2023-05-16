c84828ec3f7402d08d6f766275ed83af63749ae2
Rene Kouache <s1750463@talos.rbicl.cs.uni-frankfurt.de>
2023-05-16 14:33:38 +0200

    Hilfsfunktionen stehen

--- Assignment2/Task1/main.c
+++ Assignment2/Task1/main.c
@@ -29,6 +29,7 @@
 int create_random_array(int length_of_array, int array[], int rank_for_seed);
 void print_int_array(int length, int array[]);
 int compare( void const * left, void const * right );
+int extract_wanted_numbers(int LENGTH,int array_local[],int array_recieved[],int extract_left_side);
 
 int main(int argc, char* argv[ ]) 
 { 
@@ -38,6 +39,7 @@ int main(int argc, char* argv[ ])
 	int tag = 0; 
     const int LENGTH = 10; 
     int random_numbers[LENGTH];
+    int random_numbers2[LENGTH];
 
 	/* Start of MPI part*/
 
@@ -47,9 +49,94 @@ int main(int argc, char* argv[ ])
 
     
     create_random_array(LENGTH, random_numbers, my_rank);// Create an array of random numbers
+    create_random_array(LENGTH, random_numbers2, 10);// Create an array of random numbers
     print_int_array(LENGTH,random_numbers);
-    qsort( random_numbers, LENGTH, sizeof(int),compare);
+    
+    extract_wanted_numbers(LENGTH,random_numbers,random_numbers2,0);
     print_int_array(LENGTH,random_numbers);
+
+
+    // qsort( random_numbers, LENGTH, sizeof(int),compare);
+
+    // /*
+    // First we will open the persitent channels to our left and right partner.
+    // Special Case for the first an last Process
+    // */
+    // MPI_Request request_left_send = NULL;
+    // MPI_Request request_left_recieve = NULL;
+    // MPI_Request request_right_send = NULL;    
+    // MPI_Request request_right_recieve = NULL;
+    // MPI_Status status_left_send;
+    // MPI_Status status_left_recieve;
+    // MPI_Status status_right_send;    
+    // MPI_Status status_right_recieve;
+    // int recieved[LENGTH]; //Array that holds the values from Partner
+    // int combined[LENGTH*2]; //Array that holds owned and recieved Values and target fpr quicksort    
+    // if(my_rank == 0){
+    //     // has no left Partner establish only right side       
+    //     //right side
+    //     MPI_Recv_init(&recieved,LENGTH,MPI_INT,my_rank+1,0,MPI_COMM_WORLD,request_right_recieve);
+    //     MPI_Send_init(&random_numbers,LENGTH,MPI_INT,my_rank+1,0,MPI_COMM_WORLD,request_right_send);        
+    // }
+    // else if (my_rank == size-1){
+    //     //has no right Partner establish only left side
+    //     //left side
+    //     MPI_Recv_init(&recieved,LENGTH,MPI_INT,my_rank-1,0,MPI_COMM_WORLD,request_left_recieve);
+    //     MPI_Send_init(&random_numbers,LENGTH,MPI_INT,my_rank-1,0,MPI_COMM_WORLD,request_left_send);
+    // }
+    // else{
+    //     //has left an right Partner
+    //     //left side
+    //     MPI_Recv_init(&recieved,LENGTH,MPI_INT,my_rank-1,0,MPI_COMM_WORLD,request_left_recieve);
+    //     MPI_Send_init(&random_numbers,LENGTH,MPI_INT,my_rank-1,0,MPI_COMM_WORLD,request_left_send);
+    //     //right side
+    //     MPI_Recv_init(&recieved,LENGTH,MPI_INT,my_rank+1,0,MPI_COMM_WORLD,request_right_recieve);
+    //     MPI_Send_init(&random_numbers,LENGTH,MPI_INT,my_rank+1,0,MPI_COMM_WORLD,request_right_send);
+    // }
+    // /*
+    // we need n steps for worstcase array
+    // */
+    // for(int i = 0; i < size;i++){
+    //     if(i%2==0){
+    //         //We are in an even step
+    //         //Even ranks and 0 should communicate with the right partner and get the left side of the 2*Lenght Array            
+    //         //odd ranks should communicate with the left partner and hold the right side of the 2*Length Array
+    //         if(my_rank%2==0){
+    //             //We are in an even rank => communicate with right side
+    //             if(request_right_send == NULL || request_right_recieve == NULL)continue;
+    //             MPI_Start(&request_right_send);
+    //             MPI_Start(&request_right_recieve);
+    //             MPI_Wait(&request_right_recieve,&status_right_recieve);
+    //             //As soon as we recieved our Array we can already start extracting only the numbers we need
+
+    //             MPI_Wait(&request_right_send,&status_right_send);
+    //             //MAGIC
+    //         }
+    //         else{
+    //             //We are in an odd rank => communicate with left side
+    //             if(request_left_send == NULL || request_left_recieve == NULL)continue;
+    //             MPI_Start(&request_left_send);
+    //             MPI_Start(&request_left_recieve);
+    //             MPI_Wait(&request_right_recieve,&status_left_recieve);
+    //             MPI_Wait(&request_right_send,&status_left_send);
+    //             //MAGIC
+    //         }
+    //     }
+    //     else{
+    //         //We are in an odd step
+    //         //Even ranks and 0 should communicate with the left partner and get the right side of the 2*Lenght Array            
+    //         //odd ranks should communicate with the right partner and hold the left side of the 2*Length Array
+    //         if(my_rank%2==0){
+    //             //We are in an even rank
+    //         }
+    //         else{
+    //             //We are in an odd rank
+    //         }
+
+    //     }
+    // }
+
+    // print_int_array(LENGTH,random_numbers);
     
 
 
@@ -81,4 +168,28 @@ int compare( void const * left, void const * right )
   int* left_int = (int*) left;
   int* right_int = (int*) right;
   return (*left_int) > (*right_int);
+}
+int extract_wanted_numbers(int LENGTH,int array_local[],int array_recieved[],int extract_left_side){
+    //Combine both arrays
+    int combined[LENGTH+LENGTH];
+    for(int i = 0; i < LENGTH;i++){
+        combined[i] = array_local[i];
+    }
+    for(int i = LENGTH; i < LENGTH*2;i++){
+        combined[i] = array_recieved[i-LENGTH];
+    }    
+    qsort( combined, LENGTH+LENGTH, sizeof(int),compare);
+    print_int_array(LENGTH+LENGTH,combined);
+    if(extract_left_side){
+        printf("Bin hier \n");
+        for(int i = 0; i < LENGTH;i++){
+            array_local[i] = combined[i];
+        }
+    }//!!!!! Nochmal überprüfen
+    else{        
+        for(int i = LENGTH; i < LENGTH*2;i++){
+            array_local[i-LENGTH] = combined[i];
+        }
+    }
+    return 0;
 }
