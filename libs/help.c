10047f9eaa90ccce1a8ae7b147347ee6559b847b
Rene Kouache <s1750463@talos.rbicl.cs.uni-frankfurt.de>
2023-06-01 18:03:50 +0200

    Libs ausgegliedert

---
+++ libs/help.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include "help.h"
+int print_array(int array[],size_t Length){
+    
+
+}
---
+++ libs/help.h
@@ -0,0 +1,3 @@
+#include <stdlib.h>
+#include <stdio.h>
+int print_array(int array[],size_t Length);
